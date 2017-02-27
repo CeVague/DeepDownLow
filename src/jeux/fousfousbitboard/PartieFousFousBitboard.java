67e645a1bb903f593b746276b756bb47ec46725a
CeVague <cedric.vachaudez@u-psud.fr>
2017-02-27 18:34:59 +0100

    Ajout Gif
    ça servira peut etre pour plus tard. En tout cas ça me fait marrer pour
    le moment ^^

--- src/jeux/fousfousbitboard/PartieFousFousBitboard.java
+++ src/jeux/fousfousbitboard/PartieFousFousBitboard.java
@@ -1,10 +1,18 @@
 package jeux.fousfousbitboard;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import iia.jeux.modele.joueur.Joueur;
 
 public class PartieFousFousBitboard {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		
 		PlateauFousFousBitboard temp = new PlateauFousFousBitboard(new int[][]{{0,2,0,2,0,2,0,2},
 			{1,0,1,0,1,0,1,0},
@@ -31,5 +39,19 @@ public class PartieFousFousBitboard {
 		
 		System.out.println(temp.toString());
 		// TODO Auto-generated method stub
+
+
+        Icon icon = new ImageIcon("DeepDownLow.gif");
+        icon = new ImageIcon("GnaGnaGna.gif");
+        //icon = new ImageIcon("Gneeeeeeee.gif");
+        //icon = new ImageIcon("Lol.gif");
+        JLabel label = new JLabel(icon);
+
+        JFrame f = new JFrame("Animation");
+        f.getContentPane().add(label);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
 	}
 }
